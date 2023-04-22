5566803e475102f7d24d2168f25c36302d157c55
AmitT98 <AmitT98>
2023-04-22 19:16:13 +0530

    Added a readmeLocal.txt with instructions

--- new.py
+++ new.py
@@ -1,10 +1,28 @@
 import pytube
 import os
-scan = input('Enter the query')
-array = pytube.Search(scan)
-pytube.YouTube(array.results[0].watch_url).streams.get_by_itag('140').download(filename=(scan+".mp4"))
+from time import sleep
+import sys
+if(len(sys.argv) > 1):
+    print((sys.argv[1]))
+    url = sys.argv[1]
+    scan = sys.argv[2]
+    print("The saved file will be named {0}".format(scan))
+else:
+    scan = input('Enter the query')
+    array = pytube.Search(scan)
+    url = array.results[0].watch_url
+
+pytube.YouTube(url).streams.get_by_itag('140').download(filename=(scan+".mp4"))
 name1 = str("\ ".join(scan.split(" ")) + ".mp4")
 convert = "ffmpeg -i " + name1 + " " + name1[:-4] + ".mp3"  
 os.system(convert)
 os.system("rm " + name1)
+
+if(os.system("mkdir music 2>>error.txt")):
+    pass
+else:
+    print("music directory created")
+os.system("open " + name1[:-4] + ".mp3")
+sleep(5)
 os.system("python3 organize.py")
+
---
+++ readmeLocal.txt
@@ -0,0 +1,5 @@
+Instructions to use:
+0) Install pytube and ffmpeg
+1) Find the url of the video you want<youtubeurl>.
+2) Decide a name for your file<name>
+3) write the code in terminal as, python3 new.py <youtubeurl> <name> 
